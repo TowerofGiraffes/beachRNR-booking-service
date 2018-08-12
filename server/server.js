a735f438151bf957a9e8da9afeda99fa7fa4eb58
joseph <jbgomez88@gmail.com>
2018-08-11 17:04:04 -0700

    Implement router in express instance, update syntax to ES6

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-// Routes
+const router = require('./bookings/bookingsRouter');
+app.use('/api/bookings', router);
 
 module.exports = app;
