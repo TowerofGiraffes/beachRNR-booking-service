7853ea1fb5a4a62e79c1a78aff6c062d792cd39a
Joseph Gomez <jbgomez88@gmail.com>
2018-08-14 19:56:22 -0700

    Update model defs (#10)
    * Remove trailing comma
    
    * Remove individual model sync() calls
    
    * Add date field to BookingDates model
    
    * Swap hasMany() association method for belongsTo()

--- server/bookings/Bookings.js
+++ server/bookings/Bookings.js
@@ -12,22 +12,22 @@ const Bookings = sequelize.define('bookings', {
   booked_price: Sequelize.DECIMAL,
   adult_guests: Sequelize.INTEGER,
   child_guests: Sequelize.INTEGER,
-  infant_guests: Sequelize.INTEGER,
+  infant_guests: Sequelize.INTEGER
 });
-
-Bookings.sync();
 
 const BookingDates = sequelize.define('booking_dates', {
   id: {
     type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true,
-  }
+  },
+  date: Sequelize.DATE
 });
 
-BookingDates.sync();
+BookingDates.belongsTo(Bookings, {
+  foreignKey: 'booking_id',
+});
 
-Bookings.hasMany(BookingDates);
 sequelize.sync();
 
 exports.Bookings = Bookings;
