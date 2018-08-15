c94c8178a6e5a037012ad864ff6b54c87e29b00a
joseph <jbgomez88@gmail.com>
2018-08-14 20:31:24 -0700

    Implement retrieve method

--- server/bookings/bookingsController.js
+++ server/bookings/bookingsController.js
@@ -1,5 +1,16 @@
-var Bookings = require('./Bookings');
+const { Bookings, BookingDates } = require('./Bookings');
 
 exports.retrieve = (req, res) => {
-  res.sendStatus(200);
+  const listingID = req.params.listing;
+  
+  BookingDates
+    .findAll({
+      include: [{
+        model: Bookings,
+        where: { listing_id: listingID },
+        attributes: []
+      }]
+    })
+    .then(result => res.status(200).send(JSON.stringify(result)))
+    .catch(err => res.status(400).send(JSON.stringify(err)));
 };
