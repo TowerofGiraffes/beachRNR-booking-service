f0230f245de9480b9473f5a906928b11bd2f71fa
Joseph Gomez <jbgomez88@gmail.com>
2018-08-16 17:54:31 -0700

    Implement routes and controller methods (#11)
    * Implement retrieve method
    
    * Update retrieve method name, add post route controller method
    
    * Update retrieve method name
    
    * Implement book method skeleton, handle empty body edge case
    
    * Handle edge case where number of adult guests is less than 1
    
    * Set variables for db query
    
    * Implement data insertion using sequelize model methods
    
    * Add put route and controller method skeleton for updating bookings
    
    * Handle empty body edge case
    
    * Update bookedPrice calculation to include length of stay
    
    * Handle edge case where number of adults guests might be below 1
    
    * Set variables for updating db
    
    * Implement booking update using sequelize model methods
    
    * Add delete route, add deleteBooking method to controller
    
    * Implement success and error handler functions for sequelize promises

--- server/bookings/bookingsController.js
+++ server/bookings/bookingsController.js
@@ -1,5 +1,154 @@
-var Bookings = require('./Bookings');
+const { Bookings, BookingDates } = require('./Bookings');
 
-exports.retrieve = (req, res) => {
-  res.sendStatus(200);
+const successHandler = res => result => res.status(200).send(JSON.stringify(result));
+const errHandler = res => err => res.status(400).send(JSON.stringify(err));
+
+exports.retrieveBookedDates = (req, res) => {
+  const listingID = req.params.listing;
+  
+  BookingDates
+    .findAll({
+      include: [{
+        model: Bookings,
+        where: { listing_id: listingID },
+        attributes: []
+      }]
+    })
+    .then(successHandler(res))
+    .catch(errHandler(res));
+};
+
+exports.book = (req, res) => {
+  if (!req.body) {
+    const errMsg = JSON.stringify({
+      errMsg: 'No parameters provided with request.'
+    });
+
+    return res.status(400).send(errMsg);
+  }
+
+  if (typeof parseInt(req.body.guests.adults) !== 'number' || parseInt(req.body.guests.adults) < 1) {
+    const errMsg = JSON.stringify({
+      errMsg: 'Number of adult guests cannot be less than 1.'
+    });
+    
+    return res.status(400).send(errMsg);
+  }
+
+  const listingID = req.params.listing;
+  const dates = req.body.dates;
+  const guests = req.body.guests;
+
+  const nightlyPrice = 100; // TODO: make API call to inventory service for nightly price and calculate
+  const bookedPrice = nightlyPrice * (dates.length - 1) * (guests.adults + guests.children);
+
+  Bookings
+    .create({
+      listing_id: listingID,
+      user_id: 1, // typically, this value would be stored as a session variable/included in the API call, but for the purpose of this project, we'll leave it hardcoded as 1
+      booked_price: bookedPrice,
+      adult_guests: guests.adults,
+      children_guests: guests.children,
+      infant_guests: guests.infants
+    })
+    .then(result => {
+      const bookingDates = dates.map(date => ({ booking_id: result.id, date: date }));
+      
+      BookingDates
+        .bulkCreate(bookingDates)
+        .then(successHandler(res))
+        .catch(errHandler(res));
+    })
+    .catch(errHandler(res));
+};
+
+exports.updateBookng = (req, res) => {
+  if (!req.body) {
+    const errMsg = JSON.stringify({
+      errMsg: 'No parameters provided with request.'
+    });
+
+    return res.status(400).send(errMsg);
+  }
+
+  if (typeof parseInt(req.body.guests.adults) !== 'number' || parseInt(req.body.guests.adults) < 1) {
+    const errMsg = JSON.stringify({
+      errMsg: 'Number of adult guests cannot be less than 1.'
+    });
+    
+    return res.status(400).send(errMsg);
+  }
+
+  const listingID = req.params.listing;
+  const bookingID = req.body.bookingID;
+  const dates = req.body.dates;
+  const guests = req.body.guests;
+
+  const nightlyPrice = 100; // TODO: make API call to inventory service for nightly price and calculate
+  const bookedPrice = nightlyPrice * (dates.length - 1) * (guests.adults + guests.children);
+
+  Bookings
+    .update(
+      {
+        booked_price: bookedPrice,
+        adult_guests: guests.adults,
+        children_guests: guests.children,
+        infant_guests: guests.infants
+      },
+      {
+        where: {
+          user_id: 1, // typically, this value would be stored as a session variable/included in the API call, but for the purpose of this project, we'll leave it hardcoded as 1
+          id: bookingID
+        }
+      }
+    )
+    .then(result => {
+      BookingDates
+        .destroy({
+          where: {
+            booking_id: bookingID
+          }
+        })
+        .then(() => {
+          const bookingDates = dates.map(date => ({ booking_id: bookingID, date: date }));
+
+          BookingDates
+            .bulkCreate(bookingDates)
+            .then(successHandler(res))
+            .catch(errHandler(res));
+        })
+        .catch(errHandler(res));
+    })
+    .catch(errHandler(res));
+};
+
+
+exports.deleteBooking = (req, res) => {
+  if (!req.body) {
+    const errMsg = JSON.stringify({
+      errMsg: 'No parameters provided with request.'
+    });
+
+    return res.status(400).send(errMsg);
+  }
+
+  const bookingID = req.body.bookingID;
+
+  BookingDates
+    .destroy({
+      where: {
+        booking_id: bookingID
+      }
+    })
+    .then(result => {
+      Bookings
+      .destroy({
+        where: {
+          booking_id: bookingID
+        }
+      })
+      .then(successHandler(res))
+      .catch(errHandler(res));
+    })
+    .catch(errHandler(res));
 };
--- server/bookings/bookingsRouter.js
+++ server/bookings/bookingsRouter.js
@@ -2,6 +2,9 @@ const bookingsRouter = require('express').Router();
 const bookingsController = require('./bookingsController');
 
 bookingsRouter.route('/:listing')
-.get(bookingsController.retrieve);
+.get(bookingsController.retrieveBookedDates)
+.post(bookingsController.book)
+.put(bookingsController.updateBookng)
+.delete(bookingsController.deleteBooking);
 
 module.exports = bookingsRouter;
