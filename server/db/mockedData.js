3b800266fcc628b44d85cc7821e85274ebcc0fc7
joseph <jbgomez88@gmail.com>
2018-08-18 14:36:11 -0700

    Fix bug where chunk data wasn't reset on every loop, minor calcs

--- server/db/mockedData.js
+++ server/db/mockedData.js
@@ -1,8 +1,8 @@
 const moment = require('moment');
 const { Bookings, BookingDates } = require('./../bookings/Bookings');
 
-const bookings = [];
-const bookedDates = [];
+let bookings;
+let bookedDates;
 
 let listingID = 2912000;
 let bookingID = 1;
@@ -11,6 +11,9 @@ let userID = 1;
 const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const generateBookingDataChunk = async (start, stop) => {
+  bookings = [];
+  bookedDates = [];
+
   for (let i = start; i < stop; i++) {
     let firstCheckInDay = 1;
     let lastCheckInDay = 0;
@@ -30,10 +33,11 @@ const generateBookingDataChunk = async (start, stop) => {
         infant_guests: infantGuests
       });
 
-      firstCheckInDay += lastCheckInDay;
+      firstCheckInDay = lastCheckInDay + 1;
       lastCheckInDay += 30;
-      const checkIn = getRandomIntInclusive(firstCheckInDay , lastCheckInDay);
-      const checkOut = getRandomIntInclusive(2, 10) + checkIn;
+      let checkOut = getRandomIntInclusive(2, 10);
+      const checkIn = getRandomIntInclusive(firstCheckInDay , lastCheckInDay - checkOut);
+      checkOut += checkIn;
 
       for (let day = checkIn; day <= checkOut; day++) {
         const date = moment().add(day, 'days').format('YYYY-MM-DD H:m:s');
@@ -52,10 +56,8 @@ const generateBookingDataChunk = async (start, stop) => {
   await Bookings
   .bulkCreate(bookings)
   .then(async () => {
-    console.log('Bookings chunk complete!');
     await BookingDates
       .bulkCreate(bookedDates)
-      .then(result => console.log('Booked dates chunk complete!'))
       .catch(err => console.error('Error!', err));
   })
   .catch(err => console.error('Error!', err));
