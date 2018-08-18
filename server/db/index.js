16f5425fb7f488b1082a95ab8d579ac4ae4b9553
Joseph Gomez <jbgomez88@gmail.com>
2018-08-17 21:14:54 -0700

    Add environment variables for DB auth (#17)

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
+const dbUser = process.env.DB_USER || 'bookings';
+const dbPW = process.env.DB_PW || 'bookings';
 
-const sequelize = new Sequelize('bookings', 'bookings', 'bookings', {
+const sequelize = new Sequelize('bookings', dbUser, dbPW, {
   dialect: 'mysql',
   host: process.env.DB_HOST || 'localhost',
   operatorsAliases: false
