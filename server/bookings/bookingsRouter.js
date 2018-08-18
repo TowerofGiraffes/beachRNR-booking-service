120eb02eda1ce4d9bab42bebd55a8fb97307cead
joseph <jbgomez88@gmail.com>
2018-08-17 18:07:47 -0700

    Update API route path, implement route for mock data generation

--- server/bookings/bookingsRouter.js
+++ server/bookings/bookingsRouter.js
@@ -1,10 +1,14 @@
 const bookingsRouter = require('express').Router();
 const bookingsController = require('./bookingsController');
+const { generateData } = require('./../db/mockedData.js');
 
-bookingsRouter.route('/:listing')
+bookingsRouter.route('/api/bookings/:listing')
 .get(bookingsController.retrieveBookedDates)
 .post(bookingsController.book)
 .put(bookingsController.updateBookng)
 .delete(bookingsController.deleteBooking);
+
+bookingsRouter.route('/generateData')
+.get(generateData)
 
 module.exports = bookingsRouter;
