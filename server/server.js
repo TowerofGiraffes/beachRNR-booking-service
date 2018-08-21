25accb22637b8e4c50442d78baa587703fea3b29
Joseph Gomez <jbgomez88@gmail.com>
2018-08-20 19:43:24 -0700

    Add CORS headers (#40)

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const router = require('./bookings/bookingsRouter');
 app.use('/', router);
 
